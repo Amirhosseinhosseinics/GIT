83279388add7b5936989c67513f18ccd1fe1f9f4
Amirhossein <amirhosseinhosseini@gmail.com>
2024-08-07 23:26:43 +0330

    deleted text

--- e1.js
+++ e1.js
@@ -1,6 +1,5 @@
 console.log("Start");
 console.log("update") ;
-console.log("HAil Hitler");
 console.log("Amirhossein");
 console.log("Dev branch was usefull we want to merch ! ");
-console.log("merching branch ") ;
+console.log("merching branch ") ; 
